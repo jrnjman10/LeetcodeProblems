ce1c23580b577f1863c841e4dde16c28ac9338be
jrnjman10 <126359600+jrnjman10@users.noreply.github.com>
2025-12-15 15:23:58 -0500

    when I comment my cout it matches the fastest tiem complexity bang bang nice

--- LeetCode75(CPlusPlus)/Array/String/ArrayString/345ReverseVowlsOfAString.cpp
+++ LeetCode75(CPlusPlus)/Array/String/ArrayString/345ReverseVowlsOfAString.cpp
@@ -45,3 +45,80 @@ public:
     }
 };
 */
+
+/*
+
+//Second Naive solution: I think now I'm just going to try to do some sort of swapping
+//I think just swapping chars in the string and then stopping swapping when the iterators meet
+
+class Solution {
+public:
+    string reverseVowels(string s) {
+        string final = "";
+        auto forwardIterator = s.begin();
+        auto backwardIterator = s.end();
+        bool foundVowl = false;
+        while (forwardIterator != s.end()){
+            for (auto j : "aeiouAEIOU"){
+                if(*forwardIterator == j){
+                    foundVowl = true;
+                }
+            }
+
+            if(foundVowl){
+                while(backwardIterator >= s.begin() && foundVowl){
+                    backwardIterator--;
+                    for( auto j : "aeiouAEIOU"){
+                        if (*backwardIterator == j){
+                            foundVowl = false;
+                            final = final + *backwardIterator;
+                        }
+                    }
+                    }
+            } else {
+                final = final + *forwardIterator;
+            }
+            //cout << "\n" << *forwardIterator << " " << *backwardIterator;
+            forwardIterator++;
+        }
+        return final;
+    }
+};
+*/
+
+
+
+class Solution {
+public:
+    string reverseVowels(string s) {
+        auto forwardIterator = s.begin();
+        auto backwardIterator = s.end();
+        char placeholder;
+        bool foundVowl = false;
+        bool findSwap = false;
+        while (forwardIterator < backwardIterator){
+            for( auto i : "aeiouAEIOU"){
+                if(*forwardIterator == i) foundVowl = true;
+            }
+            while(foundVowl && backwardIterator >= forwardIterator){
+                backwardIterator--;
+                //cout << *backwardIterator << "\n";
+                for (auto i : "aeiouAEIOU"){
+                    if(*backwardIterator == i){
+                        foundVowl = false;
+                        findSwap = true;
+                    }
+                }
+            }
+            if(findSwap){
+                findSwap = false;
+                //cout << *forwardIterator << " swaps with " << *backwardIterator << "\n";
+                placeholder = *forwardIterator;
+                *forwardIterator = *backwardIterator;
+                *backwardIterator = placeholder;
+            }
+            forwardIterator++;
+        }
+        return s;
+    }
+};
