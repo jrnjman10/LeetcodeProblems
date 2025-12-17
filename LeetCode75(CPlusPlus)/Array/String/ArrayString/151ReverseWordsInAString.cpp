133b51679c6bb06d14483834b24f6b49ec442b24
jrnjman10 <126359600+jrnjman10@users.noreply.github.com>
2025-12-16 21:49:49 -0500

    working for the equal but probably want to restart and do it very intentially this time so I can get my loops right because right now they are a mess

--- LeetCode75(CPlusPlus)/Array/String/ArrayString/151ReverseWordsInAString.cpp
+++ LeetCode75(CPlusPlus)/Array/String/ArrayString/151ReverseWordsInAString.cpp
@@ -50,11 +50,9 @@ public:
             //I'm thinking I want something that'll act like a stack
             //To just go into a final string, but that could show some challenges
             if(*startIterPos2 != ' '){
-                cout << *startIterPos2;
                 startIterPos2++;
             }
             if(*endIterPos1 != ' '){
-                cout << *endIterPos1;
                 endIterPos1--;
             }
 
@@ -64,12 +62,53 @@ public:
             //of a word and make sure that only the begining of the words are swapped
             //and the remainder of any word is in the proper order.
             if(*startIterPos2 == ' ' && *endIterPos1 == ' '){
-                if(startIterPos2 - startIterPos1 > endIterPos2 - endIterPos1){
-                    cout << "\nfirst word is bigger\n";
+                cout << s << "\n";
+                if (startIterPos2 - startIterPos1 == endIterPos2 - endIterPos1){
+                    cout << "the same\n";
+                    placeholder = endIterPos1;
+                    endIterPos1 = endIterPos1+1;
+                    // while(endIterPos1 < endIterPos2){
+                    //     swap(*endIterPos1, *(endIterPos1+1));
+                    //     endIterPos1++;
+                    //     cout << s << "\n";
+                    // }
+                    endIterPos1 = placeholder;
 
                     placeholder = endIterPos1;
-                    //has to do with this position
                     endIterPos1++;
+                    while(endIterPos1 <= endIterPos2){
+                        swap(*endIterPos1,*startIterPos1);
+                        cout << s << "\n";
+                        endIterPos1++;
+                        startIterPos1++;
+                    }
+                    endIterPos1 = placeholder;
+
+                    while(*endIterPos1 == ' '){
+                        endIterPos1--;
+                    }
+                    endIterPos2 = endIterPos1;
+                } else if(startIterPos2 - startIterPos1 >= endIterPos2 - endIterPos1){
+                    cout << "first word is bigger\n";
+                    int difference = (startIterPos2 - startIterPos1) - (endIterPos2 - endIterPos1);
+                    cout << difference << "\n";
+                    //Trygve Jill Mack Joe
+                    //right side MackJoe_
+                    //ie Joe_
+                    placeholder = startIterPos2;
+                    while(startIterPos1 < startIterPos2){
+                        swap(*startIterPos2, *(startIterPos2-1));
+                        startIterPos2--;
+                        cout << s << "\n";
+                    }
+                    startIterPos2 = placeholder;
+                    FullWordSwapper(endIterPos1,(endIterPos1-endIterPos2));
+                    //LeftSide ygveTr_ 
+                    PartialWordSwapper(startIterPos1, (startIterPos1 - startIterPos2));
+                    placeholder = endIterPos1;
+                    //has to do with this position
+                    
+                    cout << *endIterPos1 << " end1 " << *startIterPos1 << " start1\n";
                     while(endIterPos1 <= endIterPos2){
                         swap(*endIterPos1,*startIterPos1);
                         cout << s << "\n";
@@ -86,10 +125,22 @@ public:
                     endIterPos2 = endIterPos1;
 
                 } else {
-                    cout << "\nsecond word is bigger or same\n";
-                    int difference = (endIterPos1 - endIterPos2) - (startIterPos1 - startIterPos2);
+                    cout << "second word is bigger\n";
                     //where first letter needs to go
-                    cout << "\n swap \n" << s << "\n";
+
+                     //Joe Jill Mack Trygve
+                    //Left side Joe_
+                    //ie _Joe
+                    placeholder = startIterPos2;
+                    while(startIterPos2 > startIterPos1){
+                        swap(*startIterPos2, *(startIterPos2-1));
+                        cout <<  s << "\n";
+                        startIterPos2--;
+                    }
+                    startIterPos2 = placeholder;
+                    FullWordSwapper(startIterPos1, (startIterPos1 - startIterPos2));
+                    //Rightside ygveTr_ 
+                    PartialWordSwapper(endIterPos1,(endIterPos1-endIterPos2));
                     placeholder = endIterPos1;
                     //it'll matter here too when we need to make sure there is enough 
                     //room for the word
@@ -100,12 +151,14 @@ public:
                         endIterPos1++;
                         startIterPos1++;
                     }
-                    endIterPos1 = placeholder;
+                    endIterPos1 = placeholder-1;
                     endIterPos2 = endIterPos1;
-                    while(*startIterPos2 == ' '){
-                        startIterPos2++;
-                    }
-                    startIterPos1 = startIterPos2;
+                    // while(*startIterPos2 == ' '){
+                    //     startIterPos2++;
+                    // }
+                    startIterPos1++;
+                    startIterPos2 = startIterPos1;
+                    cout << *startIterPos1 << " start1 " << *endIterPos1 << " end1\n";
                 }
                 //commence swap
                 //hand the 2 cases when the words are not equal in length
@@ -113,9 +166,12 @@ public:
         }
         return s;
     }
-    void setupSwapper(string::iterator s1, int s1Length){
+    void FullWordSwapper(string::iterator s1, int s1Length){
         //check for number of complete words (i.e spaces)
         //cycle to setup
         
+    }
+    void PartialWordSwapper(string::iterator s1, int s1Length){
+
     }
 };
