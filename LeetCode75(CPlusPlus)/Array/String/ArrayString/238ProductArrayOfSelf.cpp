5bd0731149f4153600c888b5f7f452a0f7566d83
jrnjman10 <126359600+jrnjman10@users.noreply.github.com>
2025-12-18 19:17:42 -0500

    you can just operate off the final array and keep track of the previous number product for when you go through the second array

--- LeetCode75(CPlusPlus)/Array/String/ArrayString/238ProductArrayOfSelf.cpp
+++ LeetCode75(CPlusPlus)/Array/String/ArrayString/238ProductArrayOfSelf.cpp
@@ -1,22 +1,45 @@
+// class Solution {
+// public:
+//     vector<int> productExceptSelf(vector<int>& nums) {
+//         vector<int> final(nums.size(),1);
+//         int i = 1;
+//         int j = nums.size()-2;
+//         int v = nums.size()-1;
+//         while(i < nums.size()){
+//             final[i] = nums[i-1] * final[i-1];
+//             i++;
+//         }
+//         while(j >= 0){
+//             nums[j] = nums[j+1] * nums[j];
+//             j--;
+//         }
+//         while(v >= 1){
+//             final[v-1] = nums[v] * final[v-1];
+//             v--;
+//         }
+//         return final;
+//     }
+// };
+
+
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         vector<int> final(nums.size(),1);
         int i = 1;
-        int j = nums.size()-2;
-        int v = nums.size()-1;
+        int j = nums.size()-1;
+        int prev = 1;
         while(i < nums.size()){
             final[i] = nums[i-1] * final[i-1];
             i++;
         }
+        prev = 1;
         while(j >= 0){
-            nums[j] = nums[j+1] * nums[j];
+            final[j] = prev * final[j];
+            prev = prev * nums[j];
             j--;
         }
-        while(v >= 1){
-            final[v-1] = nums[v] * final[v-1];
-            v--;
-        }
+        
         return final;
     }
 };
