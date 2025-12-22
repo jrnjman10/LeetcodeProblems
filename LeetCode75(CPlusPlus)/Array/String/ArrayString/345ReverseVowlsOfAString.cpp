307b2d8d92f7d489c93c8639d4a75c7fa7662777
jrnjman10 <126359600+jrnjman10@users.noreply.github.com>
2025-12-22 15:19:15 -0500

    previous commits were regarding 443 String compression, changed the code back to reverse Vowls

--- LeetCode75(CPlusPlus)/Array/String/ArrayString/345ReverseVowlsOfAString.cpp
+++ LeetCode75(CPlusPlus)/Array/String/ArrayString/345ReverseVowlsOfAString.cpp
@@ -90,64 +90,35 @@ public:
 
 class Solution {
 public:
-    int compress(vector<char>& chars) {
-        auto iterOri = chars.begin();
-        auto iterNew = chars.begin();
-        char curChar = *iterOri;
-        int count = 0;
-        int charCount = 1;
-        while(iterNew != chars.end()){
-            // for(auto j : chars){
-            //     cout << j;
-
-            // }
-            //cout << " " << *iterOri << " iterOri " << *iterNew << " iterNew\n";
-            if(*iterNew != curChar){
-                if(count != 1){
-                //erase the elements from 
-                //origin to New
-                //InsertNumber if count > 1
-                //count = 1
-                //iterOri = iterNew
-                //insert happens before the iterator
-                //erase is [ )
-                //set curChar
-                    chars.erase(iterOri+1,iterNew);
-                    // for(auto j : chars){
-                    //     cout << j;
-                    // }
-                    charCount++;
-                    iterNew = chars.begin()+charCount;
-                    charCount--;
-                    //cout << " " << *iterOri << " iterOri " << *iterNew << " iterNew\n";
-                    for(auto i : to_string(count)){
-                        chars.insert(chars.begin()+charCount,i);
-                        charCount++;
-                    }                       
-                    // for(auto j : chars){
-                    //     cout << j;
-                    // }
-                    // cout << "\n";
-                    //cout << " " << *iterOri << " iterOri " << *iterNew << " iterNew\n";
-                } 
-                iterNew = chars.begin()+charCount;
-                charCount++;
-                curChar = *iterNew;
-                iterOri = iterNew;
-                count = 1;
-            } else {
-                count++;
-                //cout << to_string(count) << " " << *iterOri << " iterOri " << *iterNew << " iterNew\n";
+    string reverseVowels(string s) {
+        auto forwardIterator = s.begin();
+        auto backwardIterator = s.end();
+        char placeholder;
+        bool foundVowl = false;
+        bool findSwap = false;
+        while (forwardIterator < backwardIterator){
+            for( auto i : "aeiouAEIOU"){
+                if(*forwardIterator == i) foundVowl = true;
             }
-            iterNew++;
+            while(foundVowl && backwardIterator >= forwardIterator){
+                backwardIterator--;
+                //cout << *backwardIterator << "\n";
+                for (auto i : "aeiouAEIOU"){
+                    if(*backwardIterator == i){
+                        foundVowl = false;
+                        findSwap = true;
+                    }
+                }
+            }
+            if(findSwap){
+                findSwap = false;
+                //cout << *forwardIterator << " swaps with " << *backwardIterator << "\n";
+                placeholder = *forwardIterator;
+                *forwardIterator = *backwardIterator;
+                *backwardIterator = placeholder;
+            }
+            forwardIterator++;
         }
-        if(count != 1){
-            chars.erase(iterOri+1,iterNew);
-            for(auto i : to_string(count)){
-                        //cout << i << "\n";
-                        chars.insert(chars.begin()+chars.size(),i);
-            } 
-        }
-        return chars.size();
+        return s;
     }
 };
