e34fe6a5b3d7dc515063d7c7bba5c64a7ca807fe
jrnjman10 <126359600+jrnjman10@users.noreply.github.com>
2025-12-22 15:20:22 -0500

    previous commits were mistakenly made in 345 which is now back to its original state. basically i made sure to reasssign the pointers after each erase and insert operation.

--- LeetCode75(CPlusPlus)/Array/String/ArrayString/443StringCompression.cpp
+++ LeetCode75(CPlusPlus)/Array/String/ArrayString/443StringCompression.cpp
@@ -1,59 +1,66 @@
-#include <string>
-#include <stdio.h>
-#include <vector>
-#include <iostream>
+// 
 
-using namespace std;
 
 class Solution {
 public:
     int compress(vector<char>& chars) {
-        string s{""};
-        auto iter = chars.begin();
-        char curChar = *iter;
+        auto iterOri = chars.begin();
+        auto iterNew = chars.begin();
+        char curChar = *iterOri;
         int count = 0;
-        while(iter != chars.end()){
-            cout << count << "\n";
-            if(*iter != curChar){
-                if(count == 1){
-                    s = s + curChar;    
-                } else {
-                    s = s + curChar + to_string(count);
-                }
-                //cout << s << "\n";
-                curChar = *iter;
+        int charCount = 1;
+        while(iterNew != chars.end()){
+            // for(auto j : chars){
+            //     cout << j;
+
+            // }
+            //cout << " " << *iterOri << " iterOri " << *iterNew << " iterNew\n";
+            if(*iterNew != curChar){
+                if(count != 1){
+                //erase the elements from 
+                //origin to New
+                //InsertNumber if count > 1
+                //count = 1
+                //iterOri = iterNew
+                //insert happens before the iterator
+                //erase is [ )
+                //set curChar
+                    chars.erase(iterOri+1,iterNew);
+                    // for(auto j : chars){
+                    //     cout << j;
+                    // }
+                    charCount++;
+                    iterNew = chars.begin()+charCount;
+                    charCount--;
+                    //cout << " " << *iterOri << " iterOri " << *iterNew << " iterNew\n";
+                    for(auto i : to_string(count)){
+                        chars.insert(chars.begin()+charCount,i);
+                        charCount++;
+                    }                       
+                    // for(auto j : chars){
+                    //     cout << j;
+                    // }
+                    // cout << "\n";
+                    //cout << " " << *iterOri << " iterOri " << *iterNew << " iterNew\n";
+                } 
+                iterNew = chars.begin()+charCount;
+                charCount++;
+                curChar = *iterNew;
+                iterOri = iterNew;
                 count = 1;
             } else {
                 count++;
+                //cout << to_string(count) << " " << *iterOri << " iterOri " << *iterNew << " iterNew\n";
             }
-            iter++;
+            iterNew++;
         }
-        s = s + curChar + to_string(count);
-        cout << s << " s \n";
-
-        return s.size();
+        if(count != 1){
+            chars.erase(iterOri+1,iterNew);
+            for(auto i : to_string(count)){
+                        //cout << i << "\n";
+                        chars.insert(chars.begin()+chars.size(),i);
+            } 
+        }
+        return chars.size();
     }
 };
-
-
-int main (){
-    Solution F;
-    vector<char> bud;
-    bud.emplace_back('a');
-    bud.emplace_back('a');
-    bud.emplace_back('a');
-    bud.emplace_back('b');
-    bud.emplace_back('b');
-    bud.emplace_back('c');
-    bud.emplace_back('c');
-    bud.emplace_back('c');
-    int me = F.compress(bud);
-    //cout << to_string(me) + "\n";
-    auto iter = bud.begin();
-    // while(iter != bud.end()){
-    //     cout << *iter;
-    //     iter++;
-    // }
-    // cout << "\n";
-    return 0;
-}
