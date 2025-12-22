a15f6db4e83b3cf499bacd70c20d1d6172a13b9e
jrnjman10 <126359600+jrnjman10@users.noreply.github.com>
2025-12-22 15:21:43 -0500

    put the old first code commmented back at the top again so I could see the naive solution

--- LeetCode75(CPlusPlus)/Array/String/ArrayString/443StringCompression.cpp
+++ LeetCode75(CPlusPlus)/Array/String/ArrayString/443StringCompression.cpp
@@ -1,5 +1,62 @@
-// 
+// #include <string>
+// #include <stdio.h>
+// #include <vector>
+// #include <iostream>
 
+// using namespace std;
+
+// class Solution {
+// public:
+//     int compress(vector<char>& chars) {
+//         string s{""};
+//         auto iter = chars.begin();
+//         char curChar = *iter;
+//         int count = 0;
+//         while(iter != chars.end()){
+//             cout << count << "\n";
+//             if(*iter != curChar){
+//                 if(count == 1){
+//                     s = s + curChar;    
+//                 } else {
+//                     s = s + curChar + to_string(count);
+//                 }
+//                 //cout << s << "\n";
+//                 curChar = *iter;
+//                 count = 1;
+//             } else {
+//                 count++;
+//             }
+//             iter++;
+//         }
+//         s = s + curChar + to_string(count);
+//         cout << s << " s \n";
+
+//         return s.size();
+//     }
+// };
+
+
+// int main (){
+//     Solution F;
+//     vector<char> bud;
+//     bud.emplace_back('a');
+//     bud.emplace_back('a');
+//     bud.emplace_back('a');
+//     bud.emplace_back('b');
+//     bud.emplace_back('b');
+//     bud.emplace_back('c');
+//     bud.emplace_back('c');
+//     bud.emplace_back('c');
+//     int me = F.compress(bud);
+//     //cout << to_string(me) + "\n";
+//     auto iter = bud.begin();
+//     // while(iter != bud.end()){
+//     //     cout << *iter;
+//     //     iter++;
+//     // }
+//     // cout << "\n";
+//     return 0;
+// }
 
 class Solution {
 public:
