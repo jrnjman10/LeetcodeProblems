53435ee1df50c2c5672d06057d794f3fc437061f
jrnjman10 <126359600+jrnjman10@users.noreply.github.com>
2025-12-23 14:48:23 -0500

    naive solution that passes test, but is slow

--- LeetCode75(CPlusPlus)/Array/String/TwoPointers/283MoveZeros.cpp
+++ LeetCode75(CPlusPlus)/Array/String/TwoPointers/283MoveZeros.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        auto iterPlaceholder = nums.begin();
-        auto iterBegin = nums.begin();
-        auto iterEnd = nums.end()-1;
-        while(iterBegin < nums.end()){
-            iterPlaceholder = iterBegin;
-            while(*iterEnd == 0 && iterEnd > nums.begin()){
-                iterEnd--;
+        //bump will start and slowly move along the list until
+        //we reach the end of the numbers
+        auto itbump = nums.begin();
+        auto it1 = itbump;
+        auto itnums = nums.begin();
+        auto it2 = itnums;
+        auto itend = nums.end()-1;
+        while(itbump < itend){
+            it1 = itbump;
+            if(*itbump == 0){
+                while(itbump < itend){
+                    swap(*itbump,*(itbump+1));
+                    itbump++;
+                }
+                itend--;
+                itbump = it1;
+            } else {
+                itbump++;
             }
-            while(*iterBegin == 0 && iterBegin < iterEnd){
-                swap(*iterBegin,*(iterBegin+1));
-                iterBegin++;
-            }
-            iterBegin = iterPlaceholder;
-            iterBegin++;
         }
     }
 };
