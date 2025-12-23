39fb581924eb09603eeefa6dc938b9fb1c4034be
jrnjman10 <126359600+jrnjman10@users.noreply.github.com>
2025-12-23 13:41:54 -0500

    second soluution, now passing initial test cases based on order.

--- LeetCode75(CPlusPlus)/Array/String/TwoPointers/283MoveZeros.cpp
+++ LeetCode75(CPlusPlus)/Array/String/TwoPointers/283MoveZeros.cpp
@@ -1,13 +1,19 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
+        auto iterPlaceholder = nums.begin();
         auto iterBegin = nums.begin();
         auto iterEnd = nums.end()-1;
-        while(iterBegin <= iterEnd){
-            if(*iterBegin == 0){
-                swap(*iterBegin, *iterEnd);
+        while(iterBegin < nums.end()){
+            iterPlaceholder = iterBegin;
+            while(*iterEnd == 0 && iterEnd > nums.begin()){
                 iterEnd--;
             }
+            while(*iterBegin == 0 && iterBegin < iterEnd){
+                swap(*iterBegin,*(iterBegin+1));
+                iterBegin++;
+            }
+            iterBegin = iterPlaceholder;
             iterBegin++;
         }
     }
