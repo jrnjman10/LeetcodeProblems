6d5cd42b8448eaf0a4cd44c232989899797c72a7
jrnjman10 <126359600+jrnjman10@users.noreply.github.com>
2025-12-24 18:04:04 -0500

    Just kinda done and dusted with this one

---
+++ LeetCode75(CPlusPlus)/Array/String/TwoPointers/392IsSubsequence.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        auto itS = s.begin();
+        auto itT = t.begin();
+        while(itS < s.end() and itT < t.end()){
+            if(*itT == *itS){
+                itS++;
+            }
+            itT++;
+        }
+        return (itS == s.end());
+    }
+};
