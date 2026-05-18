d3e9edf31a9345970a5517dc6c1237c8393938c3
jrnjman10 <126359600+jrnjman10@users.noreply.github.com>
2026-05-18 16:43:03 -0600

    Learned a lot of things in this problem. I learn about how powerful boththe sort and == are. I need to see about using sort more, as I tend to just write myself for that sort of thing. Need to think about it as lett of something I need to figure out and more as a tool. I also didn't know you could == with sets and vectors like that. Really power full operator.

--- LeetCode75(CPlusPlus)/Hashmap/Set/1667DetermineIfTwoStringsAreClose.cpp
+++ LeetCode75(CPlusPlus)/Hashmap/Set/1667DetermineIfTwoStringsAreClose.cpp
@@ -1,56 +1,62 @@
 class Solution {
 public:
-    bool closeStrings(string word1, string word2) {
-        /*
-        Go char by char on each string
-        as long as they have the same chars and same length the strings are close
-        */
-
-        //hash set
-        //unordered_set
-        unordered_set<char> existChar1;
-        unordered_set<char> existChar2;
-        if(word1.length() == word2.length()){
-            //while not at end of string
-            auto i = word1.begin();
-            while(i != word1.end()){
-                //if char not in unordered set emplace
-                if(existChar1.find(*i) == existChar1.end()){
-                    existChar1.emplace(*i);
-                }
-                //iterate over chars
-                //cout << *i << " \n";
-                i++;
-            }
-
-            auto j = word2.begin();
-            //while not at end of string2
-            while(j != word2.end()){
-                //if char not in unordered set emplace
-                if(existChar2.find(*j) == existChar2.end()){
-                    existChar2.emplace(*j);
-                }
-                //iterate over chars
-                //cout << *i << " \n";
-                j++;
-            }
-            auto k = existChar1.begin();
-            while(k != existChar1.end()){
-                if(existChar2.find(*k) == existChar2.end()){
-                    return false;
-                }
-                k++;
-            }
-            auto w = existChar2.begin();
-            while(w != existChar2.end()){
-                if(existChar1.find(*w) == existChar1.end()){
-                    return false;
-                }
-                w++;
-            }
-            return true;
-        } else {
-            return false;
+    bool closeStrings(string word1, string word2) {      
+        vector<int> w1Freq (26,0), w2Freq(26,0);
+        set<char> w1,w2;
+        for(char c:word1){
+            w1Freq[c-'a']++;
+            w1.insert(c);
         }
+        for(char c:word2){
+            w2Freq[c-'a']++;
+            w2.insert(c);
+        }
+        sort(w1Freq.begin(),w1Freq.end());
+        sort(w2Freq.begin(),w2Freq.end());
+        return (w1 == w2) && (w1Freq == w2Freq); 
     }
 };
+
+/*
+Looked up example solution and this one is pretty smart
+You basically get the requency of the letters in 2 sepreate vectors
+you make sure they have the same letters used
+you sort the vectors
+now compare the number of occurences and if any are off they are not alike
+otherwise true.
+class Solution {
+public:
+    bool closeStrings(std::string word1, std::string word2) {
+        std::vector<int> freq1(26, 0);
+        std::vector<int> freq2(26, 0);
+
+        for (char ch : word1) {
+            freq1[ch - 'a']++;
+        }
+
+        for (char ch : word2) {
+            freq2[ch - 'a']++;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if ((freq1[i] == 0 && freq2[i] != 0) || (freq1[i] != 0 && freq2[i] == 0)) {
+                return false;
+            }
+        }
+
+        std::sort(freq1.begin(), freq1.end());
+        std::sort(freq2.begin(), freq2.end());
+
+        for (int i = 0; i < 26; i++) {
+            if (freq1[i] != freq2[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
+
+
+
+*/
