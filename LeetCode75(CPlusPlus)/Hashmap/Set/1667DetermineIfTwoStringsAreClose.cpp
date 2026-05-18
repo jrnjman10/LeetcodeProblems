6afe4cb5aab673aa3f18cc1b7c81afc55d9eefcc
jrnjman10 <126359600+jrnjman10@users.noreply.github.com>
2026-05-18 16:04:20 -0600

    kinda spagehti coded to get to here and now it's the point where the problem explanation is where I'm not totally understanding. I have an ugly solution, but I think this solution should work, but it doesn't. Gotta circle back to the explanation.

--- LeetCode75(CPlusPlus)/Hashmap/Set/1667DetermineIfTwoStringsAreClose.cpp
+++ LeetCode75(CPlusPlus)/Hashmap/Set/1667DetermineIfTwoStringsAreClose.cpp
@@ -8,14 +8,15 @@ public:
 
         //hash set
         //unordered_set
-        unordered_set<char> existChar;
+        unordered_set<char> existChar1;
+        unordered_set<char> existChar2;
         if(word1.length() == word2.length()){
             //while not at end of string
             auto i = word1.begin();
             while(i != word1.end()){
                 //if char not in unordered set emplace
-                if(existChar.find(*i) == existChar.end()){
-                    existChar.emplace(*i);
+                if(existChar1.find(*i) == existChar1.end()){
+                    existChar1.emplace(*i);
                 }
                 //iterate over chars
                 //cout << *i << " \n";
@@ -25,13 +26,27 @@ public:
             auto j = word2.begin();
             //while not at end of string2
             while(j != word2.end()){
-                //if char not in unordered set return false
-                //cout << *j << " \n";
-                if(existChar.find(*j) == existChar.end()){
-                    return false;
+                //if char not in unordered set emplace
+                if(existChar2.find(*j) == existChar2.end()){
+                    existChar2.emplace(*j);
                 }
                 //iterate over chars
+                //cout << *i << " \n";
                 j++;
+            }
+            auto k = existChar1.begin();
+            while(k != existChar1.end()){
+                if(existChar2.find(*k) == existChar2.end()){
+                    return false;
+                }
+                k++;
+            }
+            auto w = existChar2.begin();
+            while(w != existChar2.end()){
+                if(existChar1.find(*w) == existChar1.end()){
+                    return false;
+                }
+                w++;
             }
             return true;
         } else {
