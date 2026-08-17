157736c29ed290f775c9cc8faeefa28896d2087f
jrnjman10 <jrnjman10@gmail.com>
2026-08-16 20:09:22 -0600

    This one works on the basic test cases. I think the if statements make it hard to parse so I'm hoping the simple solution is cleaner.

--- LeetCode75(CPlusPlus)/Graphs - BFS/1926NearestExitFromEntranceInMaze.cpp
+++ LeetCode75(CPlusPlus)/Graphs - BFS/1926NearestExitFromEntranceInMaze.cpp
@@ -7,7 +7,7 @@ public:
         */
 
         int sizeL = maze.size(); //
-        int sizeW = maze[0].size()-1; //gives us coords to check if valid
+        int sizeW = maze[0].size(); //gives us coords to check if valid
 
         queue<vector<int>> q;
         q.push(entrance);
@@ -15,13 +15,14 @@ public:
         //we don't check if entrance is exit
         vector<int> curr;
         bool escaped = false;
-        while(!escaped){
+        while(!q.empty()){
             curr = q.front();
             int x = curr[1];
             int y = curr[0];
+            steps = abs(y-entrance[0]) + abs(x-entrance[1]);
             vector<int> add;
-            steps++;
-            if(curr[0] == 0 || curr[0] == sizeL || curr[1] == 0 || curr[1] == sizeW && curr != entrance) escaped = true;
+            cout << curr[0] << curr[1] << " " << entrance[0] << entrance [1] << "\n";
+            if((curr[0] == 0 || curr[0] == sizeL-1 || curr[1] == 0 || curr[1] == sizeW-1) && (curr[0] != entrance[0] || curr[1] != entrance[1])) return steps;
             //up
             if(y-1 > -1 && maze[y-1][x] != '+'){
                 add.push_back(y-1);
@@ -36,11 +37,24 @@ public:
                 q.push(add);
                 add.clear();
             }
+            //right
+            if(x-1 > -1 && maze[y][x-1] != '+'){
+                add.push_back(y);
+                add.push_back(x-1);
+                q.push(add);
+                add.clear();
+            }
+            //left
+            if(x+1 < sizeW && maze[y][x+1] != '+'){
+                add.push_back(y);
+                add.push_back(x+1);
+                q.push(add);
+                add.clear();
+            }
             q.pop();
         }
 
 
-        if (!escaped) return -1;
-        return steps;
+        return -1;
     }
 };
