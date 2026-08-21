c22c90d61f3993c5283bb2b81dff99f31d932220
jrnjman10 <jrnjman10@gmail.com>
2026-08-21 13:42:02 -0600

    went back and implemented one of the other solutions that the solution I looked at suggested. Keep track of two columns at a time and look between the two. You just need the current row index -1 and the current column index-1 spots to calculate the current number. The solution went over taking it further and solving it with just one row and even just breaking it down into the math problem nCm (the choose function from stats)

--- LeetCode75(CPlusPlus)/DP - Multidimnsional/62UniquePaths.cpp
+++ LeetCode75(CPlusPlus)/DP - Multidimnsional/62UniquePaths.cpp
@@ -1,30 +1,12 @@
 class Solution {
 public:
-
-    void recurse(int& m, int& n, int& uniquePaths, int dM, int dN){
-        if(dM == m-1 && dN == n-1){
-            return;
-        }
-        
-        if(dM < m-1 && dN < n-1){
-            uniquePaths = uniquePaths+1;
-            recurse(m,n,uniquePaths,dM,dN+1);
-            recurse(m,n,uniquePaths,dM+1,dN);
-        } else if ( dM < m-1){
-            //uniquePaths++;
-            recurse(m,n,uniquePaths,dM+1,dN);
-        } else {
-            //uniquePaths++;
-            recurse(m,n,uniquePaths,dM,dN+1);
-        }
-
-    }
-
     int uniquePaths(int m, int n) {
-        int dM = 0;
-        int dN = 0;
-        int uniquePaths = 1;
-        recurse(m,n,uniquePaths,dM,dN);
-        return uniquePaths;
+        vector<vector<int>> v(2,vector<int>(n, 1));
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                v[i & 1][j] = v[i & 1][j-1] + v[i-1 & 1][j];
+            }
+        }
+        return v[m-1 & 1][n-1];
     }
 };
